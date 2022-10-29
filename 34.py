dfee91ed9e038ed3da757b394a923f9af6517ff8
Dh-Off <heonbella@naver.com>
2022-10-29 12:23:10 +0900

    Card

---
+++ 34.py
@@ -0,0 +1,10 @@
+#컴퓨터랑 내가 카드를 2장 뽑아서 두 카드의 합을 비교해서 더 높은 쪽이 이기는 프로그램
+
+import random
+
+card = range(1,21)
+cardList = list(card)
+com = random.randint(cardList)
+user = random.randint
+
+print(cardList)
